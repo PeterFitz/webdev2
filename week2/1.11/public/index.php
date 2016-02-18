6c5012f261aaf041eff0ed8cfee3ee875dd8df83
peterfitz <petermfitzgerald@hotmail.com>
2016-02-18 11:44:42 +0000

    exercise 1.11 done

---
+++ week2/1.11/public/index.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Created by IntelliJ IDEA.
+ * User: user
+ * Date: 18/02/2016
+ * Time: 11:19
+ */
+
+// set page title variable
+$pageTitle = 'home page';
+
+// load standard HTML header
+require_once '/../templates/html_header.inc';
+
+echo '<h1>Hello World</h1>';
+
+// load standard HTML footer
+require_once '/../templates/html_footer.inc';
+
+
+
+
+
+
+
